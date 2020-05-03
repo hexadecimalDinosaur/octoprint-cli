fbb14d3a3b51cc2039da2abb660b09857b65b426
Quentin Fan-Chiang <userblackbox@tutanota.com>
2020-05-03 12:16:19 -0400

    connection commands

--- octoprint-cli.py
+++ octoprint-cli.py
@@ -65,10 +65,28 @@ if caller.authTest() == False:
     sys.exit(1)
 
 args = sys.argv
-for i in range(3):
-    args[i] = args[i].lower()
+
+helpMsg = """octoprint-cli
+===============================================================================
+command line tool to control octoprint servers
+
+COMMANDS
+octoprint-cli help                      view this help message
+octoprint-cli print status              view job status
+octoprint-cli print select [file]       load file
+octoprint-cli print start               start print
+octoprint-cli print pause               pause print
+octoprint-cli print resume              resume print
+octoprint-cli print cancel              cancel print
+octoprint-cli connection status         view connection status
+octoprint-cli connection connect        connect to printer with autodetection
+octoprint-cli connection disconnect     connect to printer with manual settings"""
 
 try:
+    if args[1] == 'help':
+        print(helpMsg)
+        sys.exit(0)
+
     if args[1] == 'version':
         data=caller.getVersionInfo()
         print("OctoPrint v" + data['server'] + " - API v" + data['api'])
@@ -141,7 +159,7 @@ try:
                 print(colored("Extruder Target: ", attrs=['bold']) + str(data2['temperature']['tool0']['target'])+"°C")
                 print(colored("Bed Temp: ", attrs=['bold']) + str(data2['temperature']['bed']['actual'])+"°C")
                 print(colored("Bed Target: ", attrs=['bold']) + str(data2['temperature']['bed']['target'])+"°C")
-        exit(0)
+        sys.exit(0)
 
     elif args[1:3] == ['print', 'select']:
         try:
@@ -149,67 +167,140 @@ try:
                 args[3] = args[3][1:]
         except IndexError:
             print(colored("Not enough arguments specified", 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         code = caller.selectFile(args[3])
         if code == 404:
             print(colored('File does not exist', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         if code == 204:
             print(args[3]+" has been loaded")
         elif code != 204:
             print(colored('Unable to select file'))
-            exit(1)
+            sys.exit(1)
 
     elif args[1:3] == ['print', 'start']:
         if caller.getFile() == None:
             print(colored("No file has been loaded", 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         code = caller.printRequests('start')
         if code == 409 and caller.getState() in ('Printing', 'Paused', 'Pausing', 'Cancelling'):
             print(colored("Printer is already printing", 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         elif code == 409:
             print(colored("Unable to print", 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         elif code == 204:
             print(colored("Starting print",'green',attrs=['bold']))
-            exit(0)
+            sys.exit(0)
 
     elif args[1:3] == ['print', 'cancel']:
         code = caller.printRequests('cancel')
         if code == 409:
             print(colored('No print job is running', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         elif code == 204:
             print('Print job cancelling')
-            exit(0)
+            sys.exit(0)
         else:
             print(colored('Unable to cancel', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
     
     elif args[1:3] == ['print', 'pause']:
         code = caller.pauseRequests('pause')
         if code == 409:
             print(colored('No print job is running', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         elif code == 204:
             print('Print job pausing')
-            exit(0)
+            sys.exit(0)
         else:
             print(colored('Unable to pause', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
     
     elif args[1:3] == ['print', 'resume']:
         code = caller.pauseRequests('resume')
         if code == 409:
             print(colored('No print job is running', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
         elif code == 204:
             print('Print job resuming')
-            exit(0)
+            sys.exit(0)
         else:
             print(colored('Unable to resume', 'red', attrs=['bold']))
-            exit(1)
+            sys.exit(1)
+
+    elif args[1:3] == ['connection', 'status']:
+        data = caller.get('/api/connection')
+
+        if data['current']['state'] in ('Printing', 'Operational'):
+            print(colored("✅"+data['current']['state'], 'green', attrs=['bold']))
+            print(colored("Printer Profile: ", attrs=['bold']) + data['options']['printerProfiles'][0]['name'])
+            print(colored("Port: ", attrs=['bold']) + data['current']['port'])
+            print(colored("Baudrate: ", attrs=['bold']) + str(data['current']['baudrate']))
+        elif data['current']['state'] == 'Closed': #disconnected
+            print(colored("❌ Printer Disconnected", 'red', attrs=['bold']))
+            print(colored("Ports: ", attrs=['bold'])+",".join(data['options']['ports']))
+            print(colored("Baudrates: ", attrs=['bold'])+", ".join(list(map(str,data['options']['baudrates']))))
+        elif data['current']['state'].startswith("Error"): #connection error
+            print(colored("❌ Error", 'red', attrs=['bold']))
+            print(colored("Error: ", attrs=['bold'])+data['current']['state'][7:])
+            print(colored("Ports: ", attrs=['bold'])+",".join(data['options']['ports']))
+            print(colored("Baudrates: ", attrs=['bold'])+", ".join(list(map(str,data['options']['baudrates']))))
+
+    elif args[1:3] == ['connection', 'connect']:
+        data = {'command':'connect'}
+        port = ''
+        baudrate = 0
+        try:
+            if args[3] == 'port':
+                port = args[4]
+            if args[3] == 'baudrate':
+                baudrate = int(args[4])
+        except IndexError:
+            pass
+        except ValueError:
+            print(colored("Baudrate needs to be an integer", 'red', attrs=['bold']))
+        try:
+            if args[5] == 'port':
+                port = args[6]
+            if args[5] == 'baudrate':
+                baudrate = int(args[6])
+        except IndexError:
+            pass
+        except ValueError:
+            print(colored("Baudrate needs to be an integer", 'red', attrs=['bold']))
+        if port != '':
+            data['port'] = port
+        if baudrate != 0:
+            data['baudrate'] = baudrate
+        code = caller.post("/api/connection", data)
+        if code == 400:
+            print(colored("Port or baudrate is incorrect", 'red', attrs=['bold']))
+            sys.exit(1)
+        elif code == 204 and caller.getState().startswith("Error"):
+            print(colored("Unable to connect to printer", 'red', attrs=['bold']))
+            print(caller.getState())
+            sys.exit(1)
+        elif code == 204:
+            print(colored("Printer connected", 'green', attrs=['bold']))
+            sys.exit(0)
+        else:
+            print(colored("Unable to connect to printer", 'red', attrs=['bold']))
+            sys.exit(1)
+
+    elif args[1:3] == ['connection', 'disconnect']:
+        code = caller.post("/api/connection", {"command":"disconnect"})
+        if code == 204:
+            print("Printer disconnected")
+            sys.exit(0)
+        else:
+            print(colored("Unable to disconnect from printer", 'red', attrs=['bold']))
+            sys.exit(1)
+
+    else:
+        print(colored("Invalid arguments", 'red', attrs=['bold']))
+        sys.exit(1)
 
 except IndexError:
     print(colored("Not enough arguments specified", 'red', attrs=['bold']))
+    sys.exit(1)
