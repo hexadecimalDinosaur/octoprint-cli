862bec2f3ec53527e4a5768b81c0cbd642d9605d
UserBlackBox <userblackbox@tutanota.com>
2021-01-04 19:36:53 -0500

    create init

---
+++ octoprint-cli/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+Command line tool for controlling your OctoPrint 3D printer server
+Copyright 2020 Ivy Fan-Chiang
+Distributed under the GNU General Public License (GPL) v3 or later. See
+the file LICENSE for the full license text. This software makes use of open
+source software.
+"""
+
+__author__ = "Ivy Fan-Chiang"
+__copyright__ = "Copyright 2020 Ivy Fan-Chiang"
+__license__ = "GNU GPL v3 or later"
+__version__ = "3.1.0"
