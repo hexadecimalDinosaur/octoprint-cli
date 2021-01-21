ee3059a98144963fd8c4f0e363d92a027362062c
UserBlackBox <userblackbox@tutanota.com>
2021-01-21 17:53:10 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="octoprint-cli",
     packages=find_packages(),
-    version="3.2.0",
+    version="3.2.1",
     entry_points={
         "console_scripts": [
             "octoprint-cli=octoprint_cli.__main__:main"
